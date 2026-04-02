c8906090b85b49e791b12ce5f307bd94703a53b9
caliphsafe <84346820+caliphsafe@users.noreply.github.com>
2026-04-02 12:14:26 -0400

    Create celebrityVideos.ts

---
+++ lib/celebrityVideos.ts
@@ -0,0 +1,22 @@
+export const celebrityVideos = [
+  {
+    name: "Ghostface Killah",
+    url: "https://youtube.com/shorts/gs82wb4hvQ0",
+  },
+  {
+    name: "Jamel Shabazz",
+    url: "https://youtu.be/E6fpVO3etxM",
+  },
+  {
+    name: "Dougie Fresh",
+    url: "https://youtu.be/sslSwnDS5V8",
+  },
+  {
+    name: "Dapper Dan",
+    url: "https://youtu.be/4N8MkCMnwio",
+  },
+  {
+    name: "Sean Wotherspoon",
+    url: "https://youtube.com/shorts/YS7wCLJ6x7E",
+  },
+];
